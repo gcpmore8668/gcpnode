1178f3b6fb35c3696bda25cf2ca348cfce633853
gcpmore8668 <gcp.more8668@gmail.com>
2024-07-19 10:33:40 +0700

    Update rivalavalan.sh

--- rivalavalan.sh
+++ rivalavalan.sh
@@ -45,7 +45,6 @@ zones=(
   "europe-west4-b"
   "europe-west10-a"
   "europe-west3-c"
-  "europe-west9-a"
 )
 # Kiểm tra sự tồn tại của tổ chức
 organization_id=$(gcloud organizations list --format="value(ID)" 2>/dev/null)
